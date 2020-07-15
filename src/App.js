7ed31206ff189261d71df4bae1216889da970e44
강혜성 <ganghyeseong@MacBook-Pro.local>
2020-07-15 11:21:21 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, {Component} from 'react';
 import './App.css';
 import Customer from './components/Customet';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
 
 
+const styles = theme => ({
+root: {
+  width: '100%',
+  marginTop: theme.spacing.unit *3,
+overfolwX: "auto"
+},
+table:{
+minWidth: 1080
+
+}
+
+})
 const customers= [{
   'id': 1,
   'image':'https://placeimg.com/64/64/1',
@@ -32,8 +51,25 @@ const customers= [{
 class App extends Component {
 
   render(){
+    const {classes} = this.props;
     return(
       <div>
+        <Paper className={classes.root}> 
+        <Table className={classes.table} > 
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월이</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+
+
+         
      {customers.map(customer => {
        return (
          <Customer 
@@ -47,10 +83,13 @@ class App extends Component {
          />
        )
      })}
+      </TableBody>
+     </Table>
+     </Paper>
   </div>
     )
   }
 }
 
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customet.js
+++ src/components/Customet.js
@@ -1,51 +1,63 @@
 import React , {Component} from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 
 class Customer extends Component {
 render() {
     const {id, name, birthday, gender, job, image} = this.props;
     return(
-            <div>
-               <CustomerProfile id={id} name={name} image={image}/>
-               <CustomerInfo birthday={birthday} gender={gender} job={job}/>
-            </div>
+
+        <TableRow>
+            <TableCell>{id}</TableCell>
+            <TableCell><img src={image}  alt="profile"/></TableCell>
+            <TableCell>{name}</TableCell>
+            <TableCell>{birthday}</TableCell>
+            <TableCell>{gender}</TableCell>
+            <TableCell>{job}</TableCell>
+
+        </TableRow>
+            // <div>
+            //    <CustomerProfile id={id} name={name} image={image}/>
+            //    <CustomerInfo birthday={birthday} gender={gender} job={job}/>
+            // </div>
 
     )
 }
 }
 
 
-class CustomerProfile extends Component {
+// class CustomerProfile extends Component {
 
-    render(){
-        const {image, id, name } = this.props;
-        return(
-            <div>
-                <img src={ image} alt="profile"/>
-        <h2>{name} ({id})</h2>
+//     render(){
+//         const {image, id, name } = this.props;
+//         return(
+//             <div>
+//                 <img src={ image} alt="profile"/>
+//         <h2>{name} ({id})</h2>
 
-            </div>
+//             </div>
 
-        )
-    }
-}
-
-
-class CustomerInfo extends Component{
-    render(){
-        const {birthday, gender, job } = this.props;
-        return(
-                <div>
-                    <p>{birthday}</p>
-                    <p>{gender}</p>
-                    <p>{job}</p>
+//         )
+//     }
+// }
 
 
-                </div>
+// class CustomerInfo extends Component{
+//     render(){
+//         const {birthday, gender, job } = this.props;
+//         return(
+//                 <div>
+//                     <p>{birthday}</p>
+//                     <p>{gender}</p>
+//                     <p>{job}</p>
 
-        )
 
-    }
-}
+//                 </div>
+
+//         )
+
+//     }
+// }
 
 export default Customer;
