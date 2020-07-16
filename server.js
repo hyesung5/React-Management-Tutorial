a9ebdd815b68caa487ed2b37b8ccaa751cb313e1
강혜성 <ganghyeseong@MacBook-Pro.local>
2020-07-17 02:42:35 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- server.js
+++ server.js
@@ -24,7 +24,20 @@ const connection = mysql.createConnection({
 connection.connect();
 
 const multer = require('multer');
-const upload = multer({dest: './upload'});
+const moment =  require('moment');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, './upload');  // 파일이 저장되는 경로입니다.
+    },
+    filename: function(req, file, cb) {
+      cb(null, moment().format('YYYYMMDDHHmmss') + "_" + file.originalname);  // 저장되는 파일명
+    }
+  });
+  
+
+// const upload = multer({dest: './upload'});
+const upload = multer({storage: storage});
 
 
 
@@ -40,7 +53,7 @@ app.use('/image',express.static('./upload'));
 
 app.post('/api/customers', upload.single('image'), (req, res) => {
     let sql = 'INSERT INTO `CUSTOMER` VALUES (null, ?, ?, ?, ?, ?)';
-    let image = '/image/' + req.file.filename;
+    let image = 'http://localhost:5000/image/' + req.file.filename;
     let name = req.body.name;
     let birthday = req.body.birthday;
     let gender = req.body.gender;
